e23907e5881e183b620ad2eca6558b0651e2927c
W|H Designs <will@rhinorea.com>
2019-10-31 00:06:20 -0400

    adding changes to index page, gulpfile and the scss page.

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var sass        = require('gulp-sass');
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss', 'node_modules/fontawesome-free/scss/fontawesome.scss'])
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
         .pipe(sass())
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
